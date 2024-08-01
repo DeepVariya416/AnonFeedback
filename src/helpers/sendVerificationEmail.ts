29308842b53213f9cdfd0d5b3c8cb904408689ec
VariyaDeep <deepvariya7@gmail.com>
2024-08-01 12:30:19 +0530

    changed email name

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'deepvariya416@gmail.com',
+      from: 'anonymusfeedback.netlify.app',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
