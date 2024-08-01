4f835386345a40dc442b3d88ea191cbb8b6c6537
DeepVariya416 <deepvariya416@gmail.com>
2024-08-01 13:01:22 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Anonymus Feedback',
+  title: 'Anonymus deep Feedback',
   description: 'Real feedback from real people.',
 };
 
